bd7dc6a4c84d9aed3d39dc36c5d498838619f558
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 22:17:23 +0000

    [Cleanup] Delete copyright notice

--- Sources/Hex/Encode.swift
+++ Sources/Hex/Encode.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 typealias EncodeTable = [UInt8]
 private let table: EncodeTable = .init(("0123456789abcdef").utf8)
 
